d240d84edf410ad7913530173f490c9544f0032d
Tomasz74459 <166233708+Tomasz74459@users.noreply.github.com>
2024-04-06 17:11:47 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = "plikZadania.txt";
+
+        try
+        {
+            string[] lines = File.ReadAllLines(filePath);
+
+            foreach (string line in lines)
+            {
+                char[] characters = line.ToCharArray();
+                Array.Reverse(characters);
+
+                Console.WriteLine(new string(characters));
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Wystąpił błąd: " + ex.Message);
+        }
+    }
+}
